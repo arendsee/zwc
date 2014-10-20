12058fd3d7eced499f089bd52a6456de074c69c8
Zebulun Arendsee <arendsee@iastate.edu>
2014-10-19 23:34:07 -0500

    release 0.1.0 (first stable version)

--- wordcount.cpp
+++ wordcount.cpp
@@ -1,68 +1,76 @@
+/* wordcount - a filter for counting words in a FASTA file
+ * author - Zebulun Arendsee
+ *
+ * $ wordcount 2 < sample.faa 
+ * CA	3
+ * AA	3
+ * TA	2
+ * GA	1
+ * TG	1
+ * GT	1
+ * AT	3
+ * CG	1
+ * AC	4
+ */
+
+
 #include <stdio.h>
 #include <boost/unordered_map.hpp>
-#include <stdlib.h>
 #include <string>
+#include <string.h>
+using std::string;
+using boost::unordered_map;
+
+string VERSION = "0.1.0";
 
 int main(int argc, char ** argv){
     size_t k = 1;
-    switch (argc) {
-        case 1:
-            return 1;
-        case 2:
-            break;
-        case 3:
-            k = atoi(argv[2]);
-            break;
+    if (argc == 2){
+        if(argv[1][0] == '-'){
+            printf("zwordcount %s (faster EMBOSS wordcount)\n", VERSION.c_str()); 
+            printf("USAGE - zwordcount [word length] < d.fasta\n"); 
+            printf("INPUT - A properly formatted FASTA file\n"); 
+            return 0;
+        }
+        k = atoi(argv[1]);
     }
+    string word = string(k, 'x');
 
-    FILE * fr = fopen(argv[1], "rb");
-    size_t buffer_size = 1024;
-    char buffer[buffer_size];
-
-    std::string word;
-    for(int i = 0; i < k; i++){
-        word += "x";
-    }
-    typedef boost::unordered_map <std::string, int> hashmap;
+    typedef unordered_map <string, int> hashmap;
     hashmap counts;
+
     int seqpos = 0;
     bool is_header = false;
-    while(!feof(fr)){
-        fread(buffer, 1, buffer_size, fr); 
-        for(int i = 0; i < buffer_size; i++){
-            if(buffer[i] == 0)
+    char c;
+    while((c = fgetc(stdin)) != EOF){
+        switch (c) {
+            case '>':
+                is_header = true;
+                seqpos = 0;
                 break;
-            switch (buffer[i]) {
-                case '>':
-                    is_header = true;
-                    seqpos = 0;
+            case '\n':
+                if(is_header){
+                    is_header = false;
+                }
+                break;
+            default:
+                if(is_header)
                     break;
-                case '\n':
-                    if(is_header){
-                        is_header = false;
-                    }
-                    break;
-                default:
-                    if(is_header)
-                        break;
-                    seqpos++;
-                    for(int j = 0; j < (k-1); j++){
-                        word[j] = word[j + 1];
-                    }
-                    word[k-1] = buffer[i];
-                    if(seqpos >= k){
-                        counts[word]++;
-                    }
-                    break;
-            }
+                seqpos++;
+                for(int j = 0; j < (k-1); j++){
+                    word[j] = word[j + 1];
+                }
+                word[k-1] = c;
+                if(seqpos >= k){
+                    counts[word]++;
+                }
+                break;
         }
     }
 
     for(hashmap::iterator it = counts.begin(); it != counts.end(); it++) {
-        printf("%s %d\n", it->first.c_str(), it->second);
+        printf("%s\t%d\n", it->first.c_str(), it->second);
     }
-
-    fclose(fr);
 
     return 0;
 }
