a4f458bb73eb0a484b9654585b023efad34a9bb5
Zebulun Arendsee <arendsee@iastate.edu>
2016-10-27 12:29:52 -0500

    Add mitochindral DNA and protein data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Protein sequence from Arabidopsis thaliana mitochondria
+#' 
+#' @format Character vector
+#'
+#' @source TAIR10 Arabidopsis thaliana annotation
+"mito_aa"
+
+
+#' DNA sequence from Arabidopsis thaliana mitochondria
+#' 
+#' @format Character vector
+#'
+#' @source TAIR10 Arabidopsis thaliana annotation
+"mito_dna"
