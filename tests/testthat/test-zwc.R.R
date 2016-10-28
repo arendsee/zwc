54cfb77f9564c34083017776d609615cac51374e
Zebulun Arendsee <arendsee@iastate.edu>
2016-10-28 17:16:55 -0500

    Possible fix for travis

--- tests/testthat/test-zwc.R.R
+++ tests/testthat/test-zwc.R.R
@@ -2,10 +2,10 @@ context("zwc.R")
 
 test_that("Count '>z\nA'", {
   exp <- data.frame(word=c("A"), count=c(1))
-  expect_equal(fasta_wc(1, text='>z\nA'), exp)
+  testthat::expect_equal(fasta_wc(1, text='>z\nA'), exp)
 })
 
 test_that("Count '>z\nA'", {
   exp <- data.frame(word=c("A"), count=c(2))
-  expect_equal(fasta_wc(1, text='>z\nA\n>z\nA'), exp)
+  testthat::expect_equal(fasta_wc(1, text='>z\nA\n>z\nA'), exp)
 })
