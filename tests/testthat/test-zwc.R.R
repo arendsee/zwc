2bd2660c524ee22a1d9e181de6b110b827ea63c1
Zebulun Arendsee <arendsee@iastate.edu>
2016-10-31 01:43:50 -0500

    Fix tests: ignore class `wordcount` in comparisons

--- tests/testthat/test-zwc.R.R
+++ tests/testthat/test-zwc.R.R
@@ -2,10 +2,13 @@ context("zwc.R")
 
 test_that("Count '>z\nA'", {
   exp <- data.frame(word=c("A"), count=c(1))
-  testthat::expect_equal(fasta_wc(1, text='>z\nA'), exp)
+  # Requires `expect_equivalent`, not `expect_equal`, because `exp` does not
+  # have the class attribute (`wordcount`) that fasta_wc() returned value
+  # contains. `expext_equivalent ignores attributes.
+  testthat::expect_equivalent(fasta_wc(1, text='>z\nA'), exp)
 })
 
 test_that("Count '>z\nA'", {
   exp <- data.frame(word=c("A"), count=c(2))
-  testthat::expect_equal(fasta_wc(1, text='>z\nA\n>z\nA'), exp)
+  testthat::expect_equivalent(fasta_wc(1, text='>z\nA\n>z\nA'), exp)
 })
